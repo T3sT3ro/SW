bbf21e71295698209235108f5b870600cd2c450b
Tooster <max@polarczyk.pl>
2018-11-19 22:06:54 +0100

    Add W05 - z2.c fotoresistor with interrupts

--- W05/z2.c
+++ W05/z2.c
@@ -44,34 +44,31 @@
 //     // WGM1  = 1110 -- fast PWM top=ICR1
 //     // CS1   = 10  -- prescaler 8
 //     // ICR1  = 15624 -- próg resetu
-    
+
 //     // ADC
 //     ADMUX = _BV(REFS0);   // Vref on AVCC
-    
+
 //     ADCSRA |= _BV(ADEN) | _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2);  // enable ADC, prescaler 128
 
 //     sei();
 //     _delay_ms(2); // stabilize ADC
-    
+
 //     timer1_init()
 //     ADCSRA |= _BV(ADSC);
 //     loop_until_bit_is_clear(ADCSRA, ADIF);
 
 //     while (1) {
-        
+
 //     }
 // }
 
-#include <avr/io.h>
-#include <stdio.h>
-#include <inttypes.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <util/delay.h>
 
 #include <math.h>
-#define BAUD 9600                            // baudrate
-#define UBRR_VALUE ((F_CPU) / 16 / (BAUD)-1) // zgodnie ze wzorem
-
 #define BAUD 9600                             // baudrate
 #define UBRR_VALUE ((F_CPU) / 16 / (BAUD)-1)  // zgodnie ze wzorem
 
@@ -83,8 +80,8 @@ void uart_init() {
     UCSR0C = _BV(UCSZ00) | _BV(UCSZ01);  // ustaw format 8n1
 }
 
-int uart_transmit(char data, FILE *stream) {  // czekaj aż transmiter gotowy
-    while (!(UCSR0A & _BV(UDRE0)))            // czekaj aż transmiter gotowy
+int uart_transmit(char data, FILE *stream) {
+    while (!(UCSR0A & _BV(UDRE0)))
         ;
     UDR0 = data;
     return 0;
@@ -97,26 +94,19 @@ int uart_receive(FILE *stream) {   // odczyt jednego znaku
 }
 
 FILE uart_file;
+volatile static int val = 0;
 
-void delay(int x)
-{
-    for (int i = 0; i < x; i++)
-        _delay_ms(1);
+ISR(INT0_vect) {
+    loop_until_bit_is_clear(ADCSRA, ADIF);
+    ADCSRA |= _BV(ADSC);  // convert start
+    printf("converting...\r\n");
 }
 
-volatile static int x = 0;
-ISR(INT0_vect)
-{
-    ADCSRA |= _BV(ADSC); // wykonaj konwersję
-    printf("start knowersji\r\n");
-}
-
-ISR(ADC_vect)
-{
-    ADCSRA |= _BV(ADIF); // wyczyść bit ADIF (pisząc 1!)
-    printf("koniec knowersji\r\n");
-    x = ADC;
-    printf("%d\r\n", ADC);
+ISR(ADC_vect) {
+    printf("conversion ended.\r\n");
+    ADCSRA |= _BV(ADIF);  // clear ADIF
+    val = ADC;
+    printf("%d\r\n", val);
 }
 
 int main() {
@@ -126,27 +116,27 @@ int main() {
 
     // ADC
     ADMUX = _BV(REFS0);  // Aref
-    DIDR0 = _BV(ADC0D); 
+    DIDR0 = _BV(ADC0D);
     ADCSRA = _BV(ADEN) | _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2);  // 128
+    
     // INTERRUPTS SETUP
-    DDRB |= _BV(DDB5);    // ustaw wyzwalanie przerwania na INT0 i INT1 zboczem narastającym
-    EICRA |= _BV(ISC00) | _BV(ISC01);    // odmaskuj przerwania dla INT0 i INT1
-    EIMSK |= _BV(INT0);    // odmaskuj przerwania
+    DDRB |= _BV(DDB5);               
+    EICRA |= _BV(ISC00) | _BV(ISC01);
+    EIMSK |= _BV(INT0);              
     sei();
 
     // timer init
     ICR1 = 1024;
     TCCR1A = _BV(COM1A1) | _BV(WGM11);
-    TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS10); // | _BV(CS12);
-    DDRB |= _BV(PB1);  // ustaw pin OC1A (PB1) jako wyjście
-    //ckl
-    ADCSRA |= (1 << ADIE); // enable ADC interrupt
+    TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS10);
+    DDRB |= _BV(PB1);                            
+    
+    ADCSRA |= _BV(ADIE); // ADC interrupt enable
 
-    while (1)
-    {
-        printf("%d\r\n", x);
-        //  while (!(ADCSRA & _BV(ADIF))); // czekaj na wynik
-        //  printf("koniec");
-        delay(3000);
+    while (1) {
+        float x = (float)val;
+        x = (10000.0*x)/(1024.0-x); // from equation
+        printf("%"PRId32"\r\n", (int32_t)(x));
+        _delay_ms(1000);
     }
 }
