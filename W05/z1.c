4dab4b599f30da25a0c20018133be955adc86a61
Tooster <max@polarczyk.pl>
2018-11-19 12:01:38 +0100

    Add W05 z1.c

---
+++ W05/z1.c
@@ -0,0 +1,31 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <util/delay.h>
+
+#define LED_STATE(state) PORTB = state ? PORTB | _BV(PB5) : PORTB & ~_BV(PB5);
+
+int8_t buffer[100];
+
+ISR(TIMER2_OVF_vect) {  // on timer2 overflow
+    static int8_t intCnt = -1;
+    if (++intCnt >= 100)
+        intCnt = 0;
+    LED_STATE(buffer[intCnt]);
+    buffer[intCnt] = !(PIND & _BV(PD3));
+}
+
+int main() {
+    DDRB |= _BV(PB5);  // D13 as output
+    PORTD |= _BV(PD3);  // enable pull-up resistor on D2
+
+    // timer2 setup
+    TCCR2B = _BV(CS20) | _BV(CS21) | _BV(CS22);  // 1024
+    TIMSK2 |= _BV(TOIE2);                        // interrupt mask
+
+    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+    sei();  // set external interrupt
+    while (1) sleep_mode();
+}
